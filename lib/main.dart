74920a86267c1e54f4a40a1383ea00ea7d0362c3
Vicente Russo <vicente.russo@gmail.com>
2024-02-09 10:20:34 -0300

    simplified

--- lib/main.dart
+++ lib/main.dart
@@ -11,12 +11,10 @@ class Person {
   late String name;
 }
 
+// Initialize Isar
+final isar = Isar.openSync([PersonSchema], directory: '');
+
 void main() async {
-  runApp(MyApp());
-
-  // Initialize Isar
-  final isar = await Isar.open([PersonSchema], directory: '');
-
   // Insert a random Person at startup
   final random = Random();
   final personName = 'Person ${random.nextInt(100)}';
@@ -24,45 +22,25 @@ void main() async {
     final person = Person()..name = personName;
     await isar.persons.put(person);
   });
+
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  MyApp({Key? key}) : super(key: key);
-
-  // Reference to Isar instance
-  final Future<Isar> _isarFuture = Isar.open([PersonSchema], directory: '');
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    var persons = isar.persons.where().findAllSync();
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(title: const Text('Isar Test App')),
-        body: FutureBuilder<Isar>(
-          future: _isarFuture,
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.done && snapshot.hasData) {
-              final isar = snapshot.data!;
-              return FutureBuilder<List<Person>>(
-                future: isar.persons.where().findAll(),
-                builder: (context, snapshot) {
-                  if (snapshot.connectionState == ConnectionState.done && snapshot.hasData) {
-                    final persons = snapshot.data!;
-                    return ListView.builder(
-                      itemCount: persons.length,
-                      itemBuilder: (context, index) {
-                        return ListTile(
-                          title: Text(persons[index].name),
-                        );
-                      },
-                    );
-                  } else {
-                    return const CircularProgressIndicator();
-                  }
-                },
-              );
-            } else {
-              return const CircularProgressIndicator();
-            }
+        body: ListView.builder(
+          itemCount: persons.length,
+          itemBuilder: (context, index) {
+            return ListTile(
+              title: Text(persons[index].name),
+            );
           },
         ),
       ),
